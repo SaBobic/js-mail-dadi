6010e38a36f409471e35b6f853974e7733550af6
SaBobic <fabrizioettori@gmail.com>
2022-06-28 17:23:22 +0200

    email script with for loop

--- js/email.js
+++ js/email.js
@@ -5,12 +5,18 @@ const emails = ["a@email.com", "b@email.com", "c@email.com", "d@email.com", "e@e
 const emailInput = prompt("Inserisci la tua email per accedere", "a@email.com");
 
 // Dichiaro il messaggio di benvenuto
-let welcomeMessage = "Benvenuto";
+let welcomeMessage;
 
 // Confronto l'input dell'utente con l'array
-if (emails.includes(emailInput)){
-    console.log(welcomeMessage);
-} else {
-    welcomeMessage = "Spiacenti, la tua email non è presente nei nostri sistemi.";
-    console.log(welcomeMessage);
+
+let isValid = true;
+
+for (let i = 0; i < emails.length && isValid; i++){
+    if (emailInput === emails[i]){
+        welcomeMessage = "Benvenuto";
+        isValid = false;
+    } else{
+        welcomeMessage = "Spiacenti, la tua email non è presente nei nostri sistemi.";
+    }
 }
+console.log(welcomeMessage);
